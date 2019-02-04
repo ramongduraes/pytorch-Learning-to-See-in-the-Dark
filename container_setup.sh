8eb95524ad5ffd484bc237e5f9b6c0aa69916ff0
Ramon Duraes <ramongduraes@gmail.com>
2019-02-04 00:38:12 -0200

    Adding container setup

--- container_setup.sh
+++ container_setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get install wget
 pip install --upgrade pip
 pip install rawpy==0.10
 
@@ -10,3 +11,5 @@ git clone https://github.com/ramongduraes/pytorch-Learning-to-See-in-the-Dark.gi
 
 # DEIXEI BAIXANDO MODELOS . TESTAR AMANHÃ !
 # SE NAO FUNCIONAR COM PYTORCH, TESTAR COM TENSORFLOW E CARREGAR OS MODELOS A CADA CHAMADA DO SERVIÇO
+
+wget https://drive.google.com/uc?id=1qVYtDEObRAox8SDH4Tbqs2s117v7tFWG&export=download
